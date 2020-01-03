40f551079adab00e9d2b93d054f766593775d76b
Dioni1195 <dionisio.arango95@gmail.com>
2020-01-03 21:19:10 +0000

    Fixing path

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,37 +5,42 @@ if [ ! -x /usr/sbin/nginx ]; then
     apt-get -y install nginx
 fi
 
-if [ ! -d data/ ]; then
-    mkdir data
+if [ ! -d /data ]; then
+    mkdir /data
 fi
 
-if [ ! -d data/web_static/ ]; then
-    mkdir data/web_static/
+if [ ! -d /data/web_static/ ]; then
+    mkdir /data/web_static/
 fi
 
-if [ ! -d data/web_static/releases/ ]; then
-    mkdir data/web_static/releases/
+if [ ! -d /data/web_static/releases/ ]; then
+    mkdir /data/web_static/releases/
 fi
 
-if [ ! -d data/web_static/shared/ ]; then
-    mkdir data/web_static/shared/
+if [ ! -d /data/web_static/shared/ ]; then
+    mkdir /data/web_static/shared/
 fi
 
-if [ ! -d data/web_static/releases/test/ ]; then
-    mkdir data/web_static/releases/test/
+if [ ! -d /data/web_static/releases/test/ ]; then
+    mkdir /data/web_static/releases/test/
 fi
 
-touch data/web_static/releases/test/index.html
+touch /data/web_static/releases/test/index.html
 
-echo "This is a test" > data/web_static/releases/test/index.html
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" > /data/web_static/releases/test/index.html
 
-if [ -h data/web_static/current ]; then
-    rm data/web_static/current
+if [ -h /data/web_static/current ]; then
+    rm /data/web_static/current
 fi
 
-ln -s data/web_static/releases/test/ data/web_static/current
-chown -R ubuntu:ubuntu data
-root="root \/var\/www\/html;"
-alias="alias \/data\/web_static\/current\/hbnb_static;"
-sed -i "s/$root/$alias/g" /etc/nginx/sites-enabled/default
+ln -s /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data
+location="location \/hbnb_static {\n\talias \/data\/web_static\/current\/;\n\t}"
+sed -i "/listen [::]:80 default_server;/a $location" /etc/nginx/sites-enabled/default
 sudo service nginx restart
