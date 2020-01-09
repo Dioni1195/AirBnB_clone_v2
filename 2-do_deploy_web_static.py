814ba182dc38acdd5194e7c034f44acbc19194ea
Dioni1195 <dionisio.arango95@gmail.com>
2020-01-09 22:35:45 +0000

    Fix pep8

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,6 +8,7 @@ from os import path
 
 env.hosts = ['35.185.25.151', '34.73.128.159']
 
+
 def do_pack():
     """ Function compress the files """
     local("mkdir -p versions")
@@ -31,15 +32,14 @@ def do_deploy(archive_path):
     try:
         put(archive_path, "/tmp/{}".format(file_name))
         run("mkdir -p /data/web_static/releases/{}/".
-                       format(file_dir))
+            format(file_dir))
         run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
-                       format(file_name, file_dir))
+            format(file_name, file_dir))
         run("rm /tmp/{}".format(file_name))
         run("mv /data/web_static/releases/{}/web_static/*\
-                            /data/web_static/releases/{}/".format(
-                                    file_dir, file_dir))
+             /data/web_static/releases/{}/".format(file_dir, file_dir))
         run("rm -rf /data/web_static/releases/{}/web_static".
-                       format(file_dir))
+            format(file_dir))
         run("rm -rf /data/web_static/current")
         run("ln -s /data/web_static/releases/{}/\
                             /data/web_static/current".format(file_dir))
