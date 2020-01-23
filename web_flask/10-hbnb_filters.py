6692b530d987221511cf2eff817492fefa2169a5
Dioni1195 <dionisio.arango95@gmail.com>
2020-01-23 04:06:46 +0000

    Finish html template

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -13,23 +13,12 @@ def close(arg):
     storage.close()
 
 
-@app.route('/states', strict_slashes=False)
-def states():
+@app.route('/hbnb_filters', strict_slashes=False)
+def hbnb_filters():
     """ Lists the states """
     states = storage.all("State")
-    return render_template('7-states_list.html', states=states)
-
-
-@app.route('/states/<id>', strict_slashes=False)
-def cities_by_states(id):
-    """ Lists the cities by state ID """
-    states = storage.all("State")
-    new_obj = None
-    for key, state in states.items():
-        if state.id == id:
-            new_obj = state
-            break
-    return render_template('9-states.html', states=new_obj)
+    amenities = storage.all("Amenity")
+    return render_template('10-hbnb_filters.html', states=states, amenities=amenities)
 
 
 if __name__ == '__main__':
